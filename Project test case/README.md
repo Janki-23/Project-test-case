e6606c9b3f2a750bd021ce092756af64ea85301b
Janki-23 <janki.rockersinfo@gmail.com>
2025-09-21 17:20:35 +0530

    Delete Project test case directory

--- Project test case/README.md
+++
@@ -1,2 +0,0 @@
-# pw_equityreact
-Equity React site
--- Project test case/tests/CommonActions.ts
+++
@@ -1,5 +0,0 @@
-import { Page } from '@playwright/test';
-
-export async function navigateTo(page: Page, path: string) {
-  await page.goto(path);
-}
